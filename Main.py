e542e77188735181394b6a9724be20095e73dda4
Leona-Vo <s250471@dtu.dk>
2025-04-24 14:53:53 +0200

    evaluate remainders

--- Main.py
+++ Main.py
@@ -2,6 +2,13 @@ from Agent import Agent
 from Parser import *
 from Evaluator import *
 
+BeliefBase = Agent()
 expr = "(P ↔ (Q ∨ R))"
+expr2 = "¬P ∧ P"
+BeliefBase.expand(expr)
+BeliefBase.expand("P")
+
+print("Belief Base:", expr)
 print(find_assignments_that_make_true(expr))
+print("Consistency:", is_consistent(find_assignments_that_make_true(expr)))
 
