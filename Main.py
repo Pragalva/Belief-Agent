855a240eaedb46f74864e2eceff3467d43d197b9
Pragalva <pragalvad@gmail.com>
2025-04-24 14:51:08 +0200

    Added the CNF convertor

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Agent import Agent
 from Parser import *
 from Evaluator import *
+from CNF_converter import *
 
 belief_agent = Agent()
 
@@ -15,3 +16,5 @@ CNF = belief_agent.combine_with_and()
 print(CNF)
 print(find_assignments_that_make_true(CNF))
 print(len(find_assignments_that_make_true(CNF)))
+
+print(to_cnf(parse(tokenize(expr))))
