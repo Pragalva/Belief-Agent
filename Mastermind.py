760f40e615a2e251d36cdfa83c73351c56bffd36
Leona-Vo <s250471@dtu.dk>
2025-04-25 11:21:25 +0200

    yellow beliefs added

--- Mastermind.py
+++ Mastermind.py
@@ -4,6 +4,7 @@
 import random
 from Agent import *
 from itertools import permutations
+from collections import defaultdict
 
 def generate_combinations(num_true):
     # Define the base combination: Two True values and Two False values
@@ -28,34 +29,77 @@ def evaluate_guess(guess, code):
 
 def add_to_belief_base(guess, correct_positions, correct_colors, BeliefBase):
     """Add the evaluation of the guess to the belief base."""
-    combination = generate_combinations(correct_positions)
+    #gained info by green guesses
+    combination_g = generate_combinations(correct_positions)
 
-    belief = "("
-    for combi in combination:
+    belief_g = "("
+    for combi in combination_g:
         # Create a belief string based on the guess and the evaluation
-        belief += "("
+        belief_g += "("
         for i in range(4):
             if combi[i]:
-                belief += f"{guess[i]}{i+1} ∧ "
+                belief_g += f"{guess[i]}{i+1} ∧ "
             else:
-                belief += f"¬{guess[i]}{i+1} ∧ "
-        belief = belief[:-3]  # Remove the last " ∧ "
-        belief += ") ∨ "
-    belief = belief[:-3]  # Remove the last " ∨ "
-    belief += ")"
-    BeliefBase.expand(belief)
-    print("Belief added:", belief)   
+                belief_g += f"¬{guess[i]}{i+1} ∧ "
+        belief_g = belief_g[:-3]  # Remove the last " ∧ "
+        belief_g += ") ∨ "
+    belief_g = belief_g[:-3]  # Remove the last " ∨ "
+    belief_g += ")"
+    BeliefBase.expand(belief_g)
+    print("Green belief added:", belief_g)   
 
-    """Gained info by yellow guesses""" 
-    
+    #gained info by yellow guesses
+
+    from collections import defaultdict
+
+    combination_y = generate_combinations(correct_colors)
+
+    # Step 1: Collect positions per color
+    color_positions = defaultdict(list)
+    for i, color in enumerate(guess):
+        color_positions[color].append(i + 1)  # Positions are 1-indexed
+
+    # Step 2: Create base clauses
+    unique_clauses = []
+    for color, positions in color_positions.items():
+        # Other positions this color could be in
+        other_positions = [f"{color}{j}" for j in range(1, 5) if j not in positions]
+        negated_guesses = [f"¬{color}{j}" for j in positions]
+
+        # If there are other places it could go, include them
+        if other_positions:
+            inner_clause = f"({' ∨ '.join(other_positions)}) ∧ {' ∧ '.join(negated_guesses)}"
+        else:
+            inner_clause = ' ∧ '.join(negated_guesses)
+
+        unique_clauses.append(inner_clause)
+
+    # Step 3: Combine clauses per combination
+    belief_y = "("
+    for combi in combination_y:
+        belief_y += "("
+        for include, clause in zip(combi, unique_clauses):
+            if include:
+                belief_y += f"({clause}) ∧ "
+            else:
+                belief_y += f"¬({clause}) ∧ "
+        belief_y = belief_y[:-3] + ") ∨ "  # Remove last ' ∧ ', close group
+    belief_y = belief_y[:-3] + ")"  # Remove last ' ∨ ', close group
+
+    BeliefBase.expand(belief_y)
+    print("Yellow belief added:", belief_y)
             
 
 def next_guess(BeliefBase):
     """Generate the next guess based on the current beliefs."""
     CNF = BeliefBase.combine_with_and()
     possible_solutions = find_assignments_that_make_true(CNF)
-    guess_values = [key for key,value in possible_solutions[0].items() if value == True]
-    sorted_entries = sorted(guess_values, key=lambda x: int(x[1:])) #sort them by the second letter
+    if len(possible_solutions) > 0:
+        guess_values = [key for key,value in possible_solutions[0].items() if value == True]
+        sorted_entries = sorted(guess_values, key=lambda x: int(x[1:])) #sort them by the second letter
+    else:
+        print("Mistake in clues... No possible solutions")
+        return generate_code()
     
     new_guess = []
     for key in sorted_entries:
